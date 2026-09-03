2733858395fe0b5c24356e92e65a87c5fbb75fb8
Fernanda84 <fernandaliliam9@gmail.com>
2026-09-03 09:48:28 -0300

    commit inicial

--- ifound/views.py
+++ ifound/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def index(request):
+    query = request.GET.get('q')
+    
+    itens = [
+        {'nome': 'Nome do objeto', 'imagem': None},
+        {'nome': 'Nome do objeto', 'imagem': None},
+        {'nome': 'Nome do objeto', 'imagem': None},
+    ]
+
+    if query:
+        itens = [item for item in itens if query.lower() in item['nome'].lower()]
+
+    return render(request, 'index.html', {'itens': itens})
